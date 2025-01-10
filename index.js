242f5a5f97788e84f506969c4bdfe843e231fbbe
hamzalutfi <hamza.lutfi68@gmail.com>
2025-01-10 21:06:38 +0100

    make it betterKeeY

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app=require('./app');
 
 mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(process.env.MONGODB_URL||'mongodb://127.0.0.1:27017/hope')
     .then(() => {
         console.log('DB connection successful');
     })
